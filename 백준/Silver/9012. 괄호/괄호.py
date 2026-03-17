6f44b768afc5cce65363be35af6ee444bb441dc0
JEONMINJU <92534659+forminju@users.noreply.github.com>
2026-03-17 21:54:32 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,14 +1,14 @@
 N = int(input())
 
-for i in range(N):
+for _ in range(N):
     arr = input()
     stack = []
     is_vps = True
 
-    for char in arr:
-        if char == '(':
-            stack.append(char)
-        elif char == ')':
+    for str in arr:
+        if str == '(':
+            stack.append(str)
+        elif str == ')':
             if stack:
                 stack.pop()
             else:
@@ -16,6 +16,6 @@ for i in range(N):
                 break
 
     if is_vps and not stack:
-        print('YES')
+        print("YES")
     else:
-        print('NO')
+        print("NO")
