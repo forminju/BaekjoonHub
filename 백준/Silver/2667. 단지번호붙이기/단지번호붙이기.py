4d2d39ce3d7e374565b22e45e3bda8f59f9d7940
JEONMINJU <92534659+forminju@users.noreply.github.com>
2026-03-21 16:34:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,27 +1,33 @@
+from collections import deque
+
 n = int(input())
 
 arr = []
-for _ in range(n):
+
+for i in range(n):
     arr.append(list(map(int, input().strip())))
 
 visited = [[False] * n for _ in range(n)]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+# 새로운 단지 찾을 때마다 queue 재사용
+
 count = 0
-def dfs(x,y,visited):
-    visited[x][y] = True
+def bfs(si, sj):
     global count
+    queue = deque([(si,sj)])
+    visited[si][sj] = True
     count +=1
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if 0<=nx<n and 0<=ny<n:
-            if not visited[nx][ny] and arr[nx][ny] ==1:
-                visited[nx][ny] = True
-                dfs(nx,ny,visited)
-
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx<n and 0<=ny<n:
+                if not visited[nx][ny] and arr[nx][ny] == 1:
+                    count += 1
+                    visited[nx][ny] = True
+                    queue.append((nx,ny))
     return count
 
 apart = []
@@ -29,9 +35,10 @@ for i in range(n):
     for j in range(n):
         if not visited[i][j] and arr[i][j] == 1:
             count = 0
-            apart.append(dfs(i,j,visited))
+            apart.append(bfs(i,j))
 
 apart.sort()
+
 print(len(apart))
 for i in range(len(apart)):
     print(apart[i], end='\n')
