d3115cc744e70deb756a4481bf5db9a3fcd60c7d
JEONMINJU <92534659+forminju@users.noreply.github.com>
2026-03-21 21:37:15 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 35100 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,37 +1,27 @@
 from collections import deque
 
+def bfs(si, sj, ei, ej):
+    queue = deque([(si,sj)])
+    visited = [[0] * m for _ in range(n)]
+    visited[si][sj] = 1
 
-n, m = map(int, input().split())
-graph = []
-
-for _ in range(n):
-    graph.append(list(map(int, input()))) 
-
-# 상하좌우
-dx = [-1, 1, 0, 0] 
-dy = [0, 0, -1, 1]
-
-def bfs(x, y):
-    
-    queue = deque()
-    queue.append((x,y))
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
 
     while queue:
-        x, y = queue.popleft()
+        ni, nj = queue.popleft()
+
+        if ni == ei and nj == ej:
+            return visited[ei][ej]
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = ni + dx[i]
+            ny = nj + dy[i]
 
-            if nx<0 or nx>=n or ny<0 or ny>=m:
-                continue
-            if graph[nx][ny]==0:
-                continue
-            if graph[nx][ny]==1:
-                graph[nx][ny]=graph[x][y]+1
+            if 0<=nx<n and 0<=ny<m and arr[nx][ny] == 1 and visited[nx][ny] == 0:
                 queue.append((nx,ny))
-    return graph[n-1][m-1]
+                visited[nx][ny] = visited[ni][nj] +1
 
-
-
-print(bfs(0,0))
+n,m = map(int, input().split())
+arr = [list(map(int, input())) for _ in range(n)]
+print(bfs(0,0,n-1,m-1))
