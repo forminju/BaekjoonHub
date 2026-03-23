928a6a8e66f1e2251765d223375d20ab33c17be5
JEONMINJU <92534659+forminju@users.noreply.github.com>
2026-03-24 00:13:02 +0900

    [Gold V] Title: 로봇 청소기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,40 +1,35 @@
-n,m = map(int,input().split())
-r,c,d = map(int,input().split())
+n,m = map(int, input().split())
+r,c,d = map(int, input().split())
 arr = []
 for _ in range(n):
-    arr.append(list(map(int,input().split())))
+    arr.append(list(map(int, input().split())))
 
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
-visited = [[0]*m for _ in range(n)]
-clean_count = 0
+# 방향 : (d+3) % 4
 
-while 1:
-    if arr[r][c] == 0:
-        arr[r][c] = 2
-        clean_count += 1
+def solve(ci,cj, dr):
+    cnt = 0
+    while True:
+        if arr[ci][cj] == 0:
+            arr[ci][cj] = 2
+            cnt +=1
 
-    found_unclean = False
-    for i in range(4):
-        nx = r + dx[i]
-        ny = c + dy[i]
-
-        if nx >= 0 and nx < n and ny >= 0 and ny < m:
-            if arr[nx][ny] == 0:
-                found_unclean = True
+        flag = 1
+        for _ in range(4):
+            dr = (dr+3)%4
+            ni,nj = ci+dx[dr], cj+dy[dr]
+            if 0<=ni<n and 0<=nj<m and arr[ni][nj] == 0:
+                ci, cj = ni,nj
+                flag = 0
                 break
 
-    if not found_unclean:
-        bx, by = r-dx[d], c-dy[d]
+        if flag == 1:
+            bi, bj = ci-dx[dr], cj-dy[dr]
+            if 0<=bi<n and 0<=bj<m and arr[bi][bj] != 1:
+                    ci,cj = bi,bj
 
-        if 0<=bx<n and 0<=by<m and arr[bx][by] !=1:
-            r,c = bx,by
-        else:
-            break
-    else:
-        d = (d+3) % 4
-        nx, ny = r+dx[d], c+dy[d]
-        if 0<=nx<n and 0<=ny<m and arr[nx][ny] == 0:
-            r,c = nx,ny
+            else:
+                return cnt
 
-print(clean_count)
+print(solve(r,c,d))
